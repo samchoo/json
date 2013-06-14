63d5a0cd8a8956d70487631a9506d0c333b61030
khoa bui <averageazn1337@gmail.com>
2013-06-14 16:55:04 -0500

    d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   def index
     require 'open-uri'
     require 'json'
     url = "http://data.austintexas.gov/resource/ecmv-9xxi.json?" + "$offset=1001"
+    @result = JSON.parse(open(url).read) 
+    @result.each do |single|
+      single.each  do |a , b|
+        
+        
+    end   
 
-@result = JSON.parse(open(url).read)
 #@result = @result[0]
   end
-  
+  end
 end
