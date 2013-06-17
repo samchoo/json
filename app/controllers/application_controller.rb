ed8d3bdf2ec6eb8a5e3260ff7fd17de62f2c485c
Sam Choo <samchoo@outlook.com>
2013-06-17 09:54:03 -0500

    stores 1000 records into sqlite database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,55 +5,60 @@ class ApplicationController < ActionController::Base
     require 'open-uri'
     require 'json'
     url = "http://data.austintexas.gov/resource/ecmv-9xxi.json?" + "$offset=1001"
-    @result = JSON.parse(open(url).read) 
-    @result.each do |single|
-      @restaurant=Restaurant.new
+    result = JSON.parse(open(url).read) 
+    result.each do |single|
+    restaurant=Restaurant.new
       single.each  do |a , b|
         if a == "inspection_date"
-          @inspection_date=b
+          restaurant.inspection_date = b
         end
-        if  a== "zip_code"
-          @zip=b
+        if  a == "zip_code"
+          restaurant.zip = b
           
         end
-        if a== "address"
+        if a == "restaurant_name"
+          restaurant.name = b
+          
+        end
+        if a == "score"
+          restaurant.score=b
+        end
+        if a == "address"
           b.each do |key, val| 
-           if key =="needs_recoding"
-               @recoding= val
-               
+           if key == "needs_recoding"
+               restaurant.recoding= val
                 end
            if key == "longitude"
-               @longitude = val
-                  
+               restaurant.longitude = val
            end  
           if key == "latitude" 
-                @latitude= val
-                 
+                restaurant.latitude= val
               end  
           if key =="human_address"
              address_split=val.split(',')
+ 
                 address_split.each do |values| 
-                    address_core=values.split(":") 
-                    if address_core[0]== '{"address"'
-                        @human_address= address_core[1].to_s.delete('"')
+                    address_core = values.split(":") 
+                    if address_core[0] == '{"address"'
+                        restaurant.human_address= address_core[1].to_s.delete('"')
                        
                        end
-                    if address_core[0]== '"city"' 
-                        @city= address_core[1].to_s.delete('"')
+                    if address_core[0] == '"city"' 
+                        restaurant.city = address_core[1].to_s.delete('"')
                          
                        end 
-                      if address_core[0]== '"state"' 
-                        @state= address_core[1].to_s.delete('"') 
+                      if address_core[0] == '"state"' 
+                        restaurant.state = address_core[1].to_s.delete('"') 
                          
-                      end  
+                      end  #end if
                     end 
                 end 
                end 
             end 
           end
-          
+        restaurant.save   
          end
-        @restaurant.save 
+        
        end
   end 
  
