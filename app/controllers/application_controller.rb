5302ff9be3c11ff5fed77aa2a0a0b2035d4c217e
khoa <averageazn1337@gmail.com>
2013-06-17 01:23:56 -0500

    move to model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,54 @@ class ApplicationController < ActionController::Base
     url = "http://data.austintexas.gov/resource/ecmv-9xxi.json?" + "$offset=1001"
     @result = JSON.parse(open(url).read) 
     @result.each do |single|
+      @restaurant=Restaurant.new
       single.each  do |a , b|
-        
-        
-    end   
-
-#@result = @result[0]
-  end
-  end
-end
+        if a == "inspection_date"
+          @inspection_date=b
+        end
+        if  a== "zip_code"
+          @zip=b
+          
+        end
+        if a== "address"
+          b.each do |key, val| 
+           if key =="needs_recoding"
+               @recoding= val
+               
+                end
+           if key == "longitude"
+               @longitude = val
+                  
+           end  
+          if key == "latitude" 
+                @latitude= val
+                 
+              end  
+          if key =="human_address"
+             address_split=val.split(',')
+                address_split.each do |values| 
+                    address_core=values.split(":") 
+                    if address_core[0]== '{"address"'
+                        @human_address= address_core[1].to_s.delete('"')
+                       
+                       end
+                    if address_core[0]== '"city"' 
+                        @city= address_core[1].to_s.delete('"')
+                         
+                       end 
+                      if address_core[0]== '"state"' 
+                        @state= address_core[1].to_s.delete('"') 
+                         
+                      end  
+                    end 
+                end 
+               end 
+            end 
+          end
+          
+         end
+        @restaurant.save 
+       end
+  end 
+ 
+ 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ActiveRecord::Base
-  # attr_accessible :title, :body
+   attr_accessible :human_address, :score, :zip, :inspection_date, :name
+   attr_accessible :city, :state, :recoding, :latitude, :longitude
 end
